567edf6f459ae07b120cf299490d3a4d1535dc0c
beofrid <beofrid@gmail.com>
2025-09-01 23:08:24 -0300

    better script and add columns

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
-
-
-
-function onClickBruno (){
-    var todaysTime = document.getElementById("todaysTime");
-    var todaysDay = document.getElementById("todaysDay");
-
+function onClick(username)
+{
     var timeNow = new Date();
-    todaysTime.innerText = ` ${timeNow.getHours()}:${timeNow.getMinutes()}`
-    todaysDay.innerText = ` ${timeNow.getDay()}/${timeNow.getMonth()+1}`
-    
+    var todaysDay = `${dateFormat(timeNow.getDay())}/${timeNow.getMonth()+1}/${timeNow.getFullYear()}`;
+    var todaysTime = `${timeNow.getHours()}:${timeNow.getMinutes()}`;
 
+    var user = username.innerText;
+    fetch
+    (
+        "https://script.google.com/macros/s/AKfycbz0y81iMIL5gt2vPIWSA8gAQ_gMKTiBglBIZ-h0pMCANxtg5ESPMAFfPku9fj00RRw/exec", 
+        {
+            method: "POST",
+            body: JSON.stringify
+            ({ 
+                    date: todaysDay,
+                    hour: todaysTime,
+                    user: user
 
-    fetch("https://script.google.com/macros/s/AKfycbz0y81iMIL5gt2vPIWSA8gAQ_gMKTiBglBIZ-h0pMCANxtg5ESPMAFfPku9fj00RRw/exec", {
-    method: "POST",
-    body: JSON.stringify({ 
-        timestamp: "baringogela",
-        user: "Bruno"
-    
-    })
-  })
-
+            })
+        }
+    );
 }
 
-function onClickDiego (){
+function dateFormat (date)
+{
+    if (date.lenght>1)
+    {
+        return date;
+    }
+    else
+    {
+        return `0${date}`
 
-    var timeNow = new Date();
-    var todaysTime = `${timeNow.getHours()}:${timeNow.getMinutes()}`
-    var todaysDay = `${timeNow.getDay()}/${timeNow.getMonth()+1}`
-
-    var timeData = "Diego | " + todaysTime + " | " + todaysDay
-
-
-    fetch("https://script.google.com/macros/s/AKfycbz0y81iMIL5gt2vPIWSA8gAQ_gMKTiBglBIZ-h0pMCANxtg5ESPMAFfPku9fj00RRw/exec", {
-    method: "POST",
-    body: JSON.stringify({ timestamp: timeData })
-  })
+    }
 
 }
-
-
-
-// https://chatgpt.com/share/68b63c71-ddf4-8002-a2c4-08bc03219242
-
-
-//https://docs.google.com/spreadsheets/d/1rfhXNSSPc2y1MLK0BW6XFygbqXNuXcTsqSvYo1v0doo/edit?usp=sharing
-
-
